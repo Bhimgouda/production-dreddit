bcca7a46dc3c20ab069542593e2eabcfa9c5bb8a
sarvalabs-prakhar <prakhar.srivastava@sarva.ai>
2024-01-20 17:51:25 +0530

    feat: add show more button

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Loader from "./Loader";
+import { INITIAL_POSTS } from "./utils/constants";
 
 const Dashboard = ({
   setIsNewPostFormOpen,
@@ -11,15 +12,26 @@ const Dashboard = ({
   handleUpvote,
   handleDownvote,
 }) => {
+  const [visiblePosts, setVisiblePosts] = useState(INITIAL_POSTS);
+  const [loadingMore, setLoadingMore] = useState(false);
   const popupHandler = () => {
     setIsNewPostFormOpen(true);
   };
+  async function loadMorePosts() {
+    setLoadingMore(true);
+    setTimeout(() => {
+      setVisiblePosts(visiblePosts + 2);
+      setLoadingMore(false);
+    }, 300);
+  }
 
   return (
     <section className="middleSection iPadView">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-12 col-sm-3 col-md-3">{/* Voters Leaderboard */}</div>
+          <div className="col-12 col-sm-3 col-md-3">
+            {/* Voters Leaderboard */}
+          </div>
           <div className="col-12 col-sm-6 col-md-6 orderTop">
             <div className="postBox">
               <div className="postRight">
@@ -28,26 +40,54 @@ const Dashboard = ({
                   onClick={logicDriver ? popupHandler : showLoginModal}
                 >
                   <div className="yourProfile">{/* Profile Pic */}</div>
-                  <input type="text" className="postText" placeholder="Create New Post" />
+                  <input
+                    type="text"
+                    className="postText"
+                    placeholder="Create New Post"
+                  />
                 </div>
               </div>
             </div>
 
             {!loadingPost ? (
-              posts.map((post, index) => (
-                <Post
-                  id={index}
-                  key={index}
-                  handleUpvote={handleUpvote}
-                  handleDownvote={handleDownvote}
-                  post={post}
-                />
-              ))
+              posts
+                ?.slice(0, visiblePosts)
+                .map((post, index) => (
+                  <Post
+                    id={index}
+                    key={index}
+                    handleUpvote={handleUpvote}
+                    handleDownvote={handleDownvote}
+                    post={post}
+                  />
+                ))
             ) : (
               <div style={{ marginTop: "25px" }}>
                 <Loader loading={loadingPost} size={"25px"} color={"#fff"} />
               </div>
             )}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: "15px",
+              }}
+            >
+              {!loadingPost && posts.length > visiblePosts ? (
+                <button
+                  onClick={loadMorePosts}
+                  disabled={loadingMore}
+                  className="btn-container"
+                >
+                  {loadingMore ? (
+                    <Loader loading={true} size={"25px"} color={"#fff"} />
+                  ) : (
+                    "Show more"
+                  )}
+                </button>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
